be35f400418712d604099a9bd01cb66685901b28
magomimmo <mimmo.cosenza@gmail.com>
2013-09-26 23:29:05 +0200

    updated to cljsbuild 0.3.3 and changed group-id to adhere to clojars naming convention

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject core.async "0.1.0-SNAPSHOT"
+(defproject org.clojars.magomimmo/core.async "0.1.0-SNAPSHOT"
   :description "Facilities for async programming and communication in Clojure"
-  :url "https://github.com/clojure/core.async"
+  :url "https://github.com/magomimmo/core.async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.2.0"
   :parent [org.clojure/pom.contrib "0.1.2"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1878"]]
@@ -13,7 +14,7 @@
   :java-source-paths ["src/main/java"]
   :profiles {:dev {:source-paths ["examples"]}}
 
-  :plugins [[lein-cljsbuild "0.3.2"]]
+  :plugins [[lein-cljsbuild "0.3.3"]]
 
   :cljsbuild
   {:builds
